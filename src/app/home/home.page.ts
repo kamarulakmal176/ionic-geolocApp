42704d155c22e5a113ccada6fad264eaff17955a
Kamarul Akmal <kamarul2893@gmail.com>
2024-08-17 16:20:10 +0800

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { Browser } from '@capacitor/browser';
+
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  currentCoordinates: { latitude: number; longitude: number } | null = null;
+
+  constructor() {}
+
+  async getCurrentPosition() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      this.currentCoordinates = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      };
+    } catch (error) {
+      console.error('Error getting location', error);
+    }
+  }
+  
+  async openWebPage() {
+    await Browser.open({ url: 'https://ionicframework.com' });
+  }
+  
+ 
+
+}
